460ebd8afebe2ee1e3dd7597d204e0db595f7819
Florent BOURASSE <fbourasse@jahia.com>
2026-04-15 21:02:13 +0200

    correction linx reported issues related to Cypress tests
    Signed-off-by: Florent BOURASSE <fbourasse@jahia.com>

--- tests/cypress/e2e/01-whatIsMyPublicServerIp.cy.ts
+++ tests/cypress/e2e/01-whatIsMyPublicServerIp.cy.ts
@@ -1,49 +1,46 @@
-import {DocumentNode} from 'graphql'
+import {DocumentNode} from 'graphql';
 
 describe('What Is My Public Server IP', () => {
-    const adminPath = '/jahia/administration/whatIsMyPublicServerIp'
+    const adminPath = '/jahia/administration/whatIsMyPublicServerIp';
     // IPv4 or IPv6
-    const ipPattern = /^(\d{1,3}\.){3}\d{1,3}$|^([0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}$/
-    let getWhatIsMyPublicServerIp: DocumentNode
-
-    getWhatIsMyPublicServerIp = require('graphql-tag/loader!../fixtures/graphql/query/getWhatIsMyPublicServerIp.graphql')
+    const ipPattern = /^(\d{1,3}\.){3}\d{1,3}$|^([0-9a-fA-F]{0,4}:){2,7}[0-9a-fA-F]{0,4}$/;
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const getWhatIsMyPublicServerIp: DocumentNode = require('graphql-tag/loader!../fixtures/graphql/query/getWhatIsMyPublicServerIp.graphql');
 
     before(() => {
-        cy.login()
-    })
+        cy.login();
+    });
 
     it('check page loads and renders the IP card', () => {
-        cy.login()
-        cy.visit(adminPath)
-        
-        
-    })
+        cy.login();
+        cy.visit(adminPath);
+    });
 
     it('displays a valid public IP address', () => {
-        cy.login()
-        cy.visit(adminPath)
+        cy.login();
+        cy.visit(adminPath);
 
         cy.get('[class*="ipValue"]')
             .invoke('text')
             .invoke('trim')
-            .should('match', ipPattern)
-    })
+            .should('match', ipPattern);
+    });
 
     it('fetches public IP via GraphQL API', () => {
-        cy.login()
+        cy.login();
         cy.apollo({query: getWhatIsMyPublicServerIp})
             .its('data.whatIsMyPublicServerIp')
             .should('be.a', 'string')
-            .and('match', ipPattern)
-    })
+            .and('match', ipPattern);
+    });
 
     it('IP displayed in UI matches the GraphQL API response', () => {
-        cy.login()
+        cy.login();
         cy.apollo({query: getWhatIsMyPublicServerIp})
             .its('data.whatIsMyPublicServerIp')
             .then((ip: string) => {
-                cy.visit(adminPath)
-                cy.get('[class*="ipValue"]').should('contain.text', ip)
-            })
-    })
-})
+                cy.visit(adminPath);
+                cy.get('[class*="ipValue"]').should('contain.text', ip);
+            });
+    });
+});
